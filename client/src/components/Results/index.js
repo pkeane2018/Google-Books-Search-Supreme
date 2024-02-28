a3d99db98f116c03edee23172455251fe4290fd9
pkeane2018 <pkeane92@yahoo.com>
2024-02-28 17:53:16 -0600

    button text change from save to saved on click

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.css"
 
 function Results(props) {
+
+    const [savedStatus, setSavedStatus] = useState(false);
+    const {saveBook, title, author, image, description, link} = props;
 
     return (
         <div className="results-div container" style={style}>
             <div className="book-buttons">
                 <a href={props.link} target="_blank"><button type="button" className="btn btn-secondary">View</button></a>{"\u00a0"}
-                <button type="button" className="btn btn-secondary" style={style} onClick={() => props.saveBook(props.title, props.author, props.image, props.description, props.link)}>Save</button>
+                <button type="button" className="btn btn-secondary" style={style} onClick={() => handleClick(saveBook, savedStatus, setSavedStatus, title, author, image, description, link)}>
+                    {savedStatus ? "Saved" : "Save"}
+                </button>
             </div>
             <h2>{props.title}</h2>
             <p>{props.author}</p>
@@ -18,5 +23,14 @@ function Results(props) {
     )
 
 };
+
+function handleClick(saveBook, savedStatus, setSavedStatus, title, author, image, description, link ) {
+
+    if (savedStatus === false){
+        saveBook(title, author, image, description, link);
+        setSavedStatus(true);
+    }
+
+}
 
 export default Results;
